576780bf8ec11edfe4905ff84c9834da71e72b05
victorluadev <streamyodin@gmail.com>
2022-06-02 23:10:52 -0300

    feat: implements TDD integration tests

--- src/main/java/com/victor/library/model/entity/Book.java
+++ src/main/java/com/victor/library/model/entity/Book.java
@@ -5,13 +5,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_book")
 public class Book {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    @Column(name = "nm_title")
     private String title;
+
+    @Column(name = "nm_author")
     private String author;
+
+    @Column(name = "ds_isbn")
     private String isbn;
 }
--- src/test/java/com/victor/library/model/repository/BookRepositoryTest.java
+++ src/test/java/com/victor/library/model/repository/BookRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.victor.library.model.repository;
 
+import com.victor.library.model.entity.Book;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,11 +29,32 @@ public class BookRepositoryTest {
 
         // cenário
         String isbn = "1234";
+        Book book = Book.builder()
+                .title("Contos")
+                .author("Victor")
+                .isbn("1234")
+                .build();
+
+        entityManager.persist(book);
 
         // execução
         boolean exists = repository.existsByIsbn(isbn);
 
         // verificação
         assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should return false when doesnt exists a book with the ISBN provided")
+    public void returnFalseWhenIsbnDoesntExistsTest() {
+
+        // cenário
+        String isbn = "1234";
+
+        // execução
+        boolean exists = repository.existsByIsbn(isbn);
+
+        // verificação
+        assertThat(exists).isFalse();
     }
 }
