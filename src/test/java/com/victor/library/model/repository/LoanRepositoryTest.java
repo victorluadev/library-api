6ced62373306137ce7396b2616c5bb79595d104d
victorluadev <streamyodin@gmail.com>
2022-06-21 21:31:10 -0300

    feat: implements LoanRepository methods

--- src/test/java/com/victor/library/model/repository/LoanRepositoryTest.java
+++ src/test/java/com/victor/library/model/repository/LoanRepositoryTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -46,6 +48,29 @@ public class LoanRepositoryTest {
 
         // verificações
         assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should search loan by isbn or customer")
+    public void findByBookIsbnOrCustomerTest() {
+        Book book = createNewBook();
+        entityManager.persist(book);
+
+        Loan loan = Loan.builder()
+                .book(book)
+                .customer("Fulano")
+                .loanDate(LocalDate.now())
+                .build();
+
+        entityManager.persist(loan);
+
+        Page<Loan> result = repository.findByBookIsbnOrCustomer("1234", "Fulano", PageRequest.of(0, 10));
+
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent()).contains(loan);
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getTotalElements()).isEqualTo(1);
     }
 
     private Book createNewBook() {
