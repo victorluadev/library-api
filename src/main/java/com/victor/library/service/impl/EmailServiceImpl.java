fa45ed0adabb082d7777051cb63251c8116a862a
victorluadev <streamyodin@gmail.com>
2022-06-28 22:07:09 -0300

    feat: config mail properties

--- src/main/java/com/victor/library/service/impl/EmailServiceImpl.java
+++ src/main/java/com/victor/library/service/impl/EmailServiceImpl.java
@@ -1,13 +1,32 @@
 package com.victor.library.service.impl;
 
 import com.victor.library.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    @Value("${application.mail.default.remetent}")
+    private String remetent;
+
+    private JavaMailSender javaMailSender;
 
     @Override
     public void sendMails(List<Object> mailsList, String message) {
 
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(remetent);
+        mailMessage.setSubject("Livro com empréstimo atrasado");
+        mailMessage.setText(message);
+        mailMessage.setTo(mailsList.toArray(new String[mailsList.size()]));
+
+        javaMailSender.send(mailMessage);
     }
 }
